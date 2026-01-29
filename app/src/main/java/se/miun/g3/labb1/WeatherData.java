ac56ba86810d746b844930880f0073dfcf9fe009
Axel Friman <axelfriman04@gmail.com>
2026-01-29 11:46:50 +0100

    Added member variables, functions and constructors

--- app/src/main/java/se/miun/g3/labb1/WeatherData.java
+++ app/src/main/java/se/miun/g3/labb1/WeatherData.java
@@ -1,4 +1,29 @@
 package se.miun.g3.labb1;
 
 public class WeatherData {
+    private String weather;
+    private double degrees;
+
+    public WeatherData(){
+        weather = "";
+        degrees = 0.0;
+    }
+    public WeatherData(String weather, double degrees) {
+        this.weather = weather;
+        this.degrees = degrees;
+    }
+
+    public void setWeather(String s){
+        weather = s;
+    }
+    public void setDegrees(double d){
+        degrees = d;
+    }
+
+    public String getWeather(){
+        return weather;
+    }
+    public double getDegrees(){
+        return degrees;
+    }
 }
