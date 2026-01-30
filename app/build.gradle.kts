708562ad4fb11c870ab2cc9476da054508ed3e94
MikeGadget <mija2306@student.miun.se>
2026-01-30 10:27:09 +0100

    Added 8.0 compatability

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "se.miun.g3.labb1"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
--- app/src/main/java/se/miun/g3/labb1/MainActivity.java
+++ app/src/main/java/se/miun/g3/labb1/MainActivity.java
@@ -1,15 +1,13 @@
-package se.miun.g3.labb1;
+ package se.miun.g3.labb1;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
+ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
